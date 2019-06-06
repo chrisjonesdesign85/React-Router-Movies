15348db54c2c263cd33885808a43eb8e1877dd0a
Chris Jones <chrisjonesdesign85@gmail.com>
2019-06-05 22:40:59 -0400

    got app/servers running, wrapped app in Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
